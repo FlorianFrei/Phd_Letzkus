d99f105e6b5790702f0fc9d0938030323e4bd801
FlorianFrei <94467281+FlorianFrei@users.noreply.github.com>
2025-09-29 18:29:29 +0200

    Refactor data loading and alignment into helpers.R
    Moved data loading and alignment functions from 'explore data.qmd' to a new 'helpers.R' script for better modularity and code reuse. Updated 'explore data.qmd' to source these functions and made adjustments to data processing and plotting to support condition-based analysis.

---
+++ analysis/general/helpers.R
@@ -0,0 +1,140 @@
+load_data_with_brain_alignment <- function(datapath, metapath, depthpath, structure_path) {
+  library(data.table)
+  
+  # --- depth info
+  depth_json <- jsonlite::fromJSON(depthpath, simplifyMatrix = TRUE, flatten = TRUE)
+  depth <- data.table::rbindlist(depth_json[1:(length(depth_json) - 1)], fill = TRUE) %>%
+    mutate(ch = row_number())
+  
+  # --- structure tree
+  st <- fread(structure_path) %>%
+    select(id, name, acronym, depth, structure_id_path, parent_structure_id)
+  
+  depth <- depth %>%
+    left_join(st, by = c("brain_region" = "acronym"))
+  
+  # --- define areas (case_when for readability)
+  HUAC <- st %>%
+    mutate(area = case_when(
+      str_detect(structure_id_path, "/247/|/541/") ~ "AUD",
+      str_detect(structure_id_path, "/131/|/295/|/319/|/780/") ~ "AMY",
+      str_detect(structure_id_path, "/1089/") ~ "HIP",
+      str_detect(structure_id_path, "/1008/") ~ "MG",
+      str_detect(structure_id_path, "/549/") ~ "TH",
+      str_detect(structure_id_path, "/315/") ~ "Cortex",
+      TRUE ~ "undefined"
+    )) %>%
+    filter(!(area == "Cortex" & structure_id_path %in% structure_id_path[area == "AUD"])) %>%
+    filter(!(area == "TH" & structure_id_path %in% structure_id_path[area == "MG"])) %>%
+    select(area, structure_id_path)
+  
+  depth <- depth %>%
+    left_join(HUAC, by = "structure_id_path") %>%
+    select(brain_region, area, ch)
+  
+  # --- cluster meta
+  meta <- fread(metapath) %>%
+    left_join(depth, by = "ch") %>%
+    filter(bc_unitType == "GOOD") %>%
+    select(cluster_id, brain_region, area, depth)
+  
+  # --- main data
+  data <- fread(datapath) %>%
+    filter(cluster_id %in% meta$cluster_id) %>%
+    left_join(meta, by = "cluster_id") %>%
+    mutate(state = ifelse(state_name %in% c("Downsweep", "Opto_Upwsweep", "Opto_Downsweep", "Upsweep"), "Sound", state_name)) %>% mutate(trial_type = case_when(
+      trial_type ==1 ~ 'Upsweep',
+      trial_type ==2 ~ 'Downsweep',
+      trial_type ==3 ~ 'Opto_Upwsweep',
+      trial_type ==4 ~ 'Opto_Downsweep',
+    ))
+  
+  data
+}
+
+load_data <- function(datapath, metapath) {
+  
+  library(data.table)
+  
+  
+  # --- cluster meta
+  meta <- fread(metapath)  %>%
+    select(cluster_id, depth)
+  
+  # --- main data
+  data <- fread(datapath) %>%
+    filter(cluster_id %in% meta$cluster_id) %>%
+    left_join(meta, by = "cluster_id") %>%
+    mutate(state = ifelse(state_name %in% c("Downsweep", "Opto_Upwsweep", "Opto_Downsweep", "Upsweep"), "Sound", state_name)) %>% mutate(trial_type = case_when(
+      trial_type ==1 ~ 'Upsweep',
+      trial_type ==2 ~ 'Downsweep',
+      trial_type ==3 ~ 'Opto_Upwsweep',
+      trial_type ==4 ~ 'Opto_Downsweep',
+    ))
+  
+  data
+}
+
+
+Align_to_state <- function(Data, Alignment_State) {
+  filter2 <- Data %>%
+    group_by(trial_number) %>%
+    filter(state == Alignment_State) %>%
+    slice_min(time_bin) %>%
+    mutate(minT = time_bin) %>%
+    select(minT, trial_number) %>%
+    distinct()
+  
+  nearest_fixed <- function(times, fixed_times) {
+    i <- 1
+    out <- numeric(length(times))
+    
+    for (j in seq_along(times)) {
+      # move pointer forward while the next fixed time is closer
+      while (i < length(fixed_times) &&
+             abs(fixed_times[i + 1] - times[j]) < abs(fixed_times[i] - times[j])) {
+        i <- i + 1
+      }
+      out[j] <- fixed_times[i]
+    }
+    out
+  }
+  Data <- Data %>%
+    mutate(
+      nearest_fixed = nearest_fixed(Data$time_bin, filter2$minT),
+      rel_time = round(time_bin - nearest_fixed, 2)
+    )
+  Data <- Data %>%
+    arrange(time_bin) %>%
+    mutate(aligntrial = cumsum(c(TRUE, diff(rel_time) < -1)))
+  
+  Data <- Data %>%
+    group_by(aligntrial) %>%
+    mutate(trial_type = first(trial_type)) %>%
+    ungroup()
+  
+  datarange <- Data %>%
+    group_by(aligntrial) %>%
+    reframe(range = range(rel_time)) %>%
+    group_by() %>%
+    summarise(
+      common_min = max(range[c(TRUE, FALSE)]), # max of minimums
+      common_max = min(range[c(FALSE, TRUE)]) # min of maximums
+    )
+  print(paste("common rel_time:", datarange$common_min, datarange$common_max))
+  Data
+}
+
+load_from_list <- function(list_of_data, list_of_meta) {
+  all_loaded <- map2_dfr(list_of_data, list_of_meta, ~ {
+    load_data(.x, .y) %>%
+    mutate(file_id = tools::file_path_sans_ext(basename(.x)))
+  })
+  
+  data <- all_loaded %>%
+    mutate(
+      cluster_id = paste(file_id, cluster_id, sep = "_"),
+      trial_number = paste(file_id, trial_number, sep = "_")
+    ) %>%
+    separate(file_id, into = c("Animal", "condition"), sep = "_")
+}
